32382d1aaa563415eb0e515b7573727481064eff
David Barr <dpbarr@gmail.com>
2024-05-12 14:52:38 +0100

    small clean ups

--- panel-menu.sh
+++ panel-menu.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 vpn=$(ip link show | grep tun0)
 echo '<openbox_pipe_menu>'
-
+    # CLOCK
 TIME=$(date +%H:%M\ %B\ %d,\ %Y)
 
 
-  echo "<item label='$TIME'>"
+ echo "<item label='$TIME'>"
   echo '<action name="Execute"><execute>'
   echo "gnome-calendar"
   echo '</execute></action>'
   echo '</item>'
-  
-  
-  vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+      # end CLOCK
+    # VOLUME
+vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
 echo "<item label='$vol'>"
   echo '<action name="Execute"><execute>'
   echo "pavucontrol"
   echo '</execute></action>'
   echo '</item>'
-
-  
+    # END VOLUME
+  # SHOW IF VPN is ACTIVE
   if [ "$vpn" ]; 
 then
 vpnstatus="VPN Active"
@@ -32,12 +32,14 @@ echo "<item label='VPN Active &#128994;'>"
 
 
 fi
-  
-  
+    # END SHOW IF VPN is ACTIVE
+    
+# BATTERY STUFF
   
   Battery=$(acpi -b | grep "Battery" | sed -nr '/Battery/s/.*(\<[[:digit:]]+%).*/\1/p')
 echo "<item label=\"Battery: $Battery\"/>"
-
+  # END BATTERY STUFF
+  
 echo "<item label='Screenshot'>"
   echo '<action name="Execute"><execute>'
   echo "/home/david/.local/bin/grimshot.sh"
@@ -50,15 +52,17 @@ echo "<item label='Screenshot'>"
   echo "/home/david/bin/setbgz"
   echo '</execute></action>'
   echo '</item>' 
-  echo '<menu id="bluetooth" label="Bluetooth">'
+
+# BLUETOOTH STUFF
+echo '<menu id="bluetooth" label="Bluetooth">'
 bluetoothctl devices Connected | while read -r uuid
 do
 
 mac=$(echo $uuid | cut -d " " -f2)
-battery=$(bluetoothctl info $mac | grep 'Battery Percentage')
+battery=$(bluetoothctl info $mac | grep 'Battery Percentage' | cut -d" " -f4 )
+uuidc=$(echo $uuid | cut -d" " -f3 )
 
-
-echo "<item label='$uuid &#128267; $battery'>"  
+echo "<item label='$uuidc &#128267; $battery'>"  
 echo '</item>'
 done
 echo "   <separator/>"
@@ -69,15 +73,15 @@ echo "<item label='Bluetooth Settings'>"
   echo '</item>'
 
  echo '</menu> '
- 
+ # END BLUETOOTH STUFF
  
 
- 
+# NETWORK STUFF  
   echo '<menu id="Networks" label="Network">'
- 
+# super slow and ugly 
 nmcli -p device | grep 'connected' | while read -r nmc 
 do
-
+#
 echo "<item label='$nmc'>"
   echo '<action name="Execute"><execute>'
   echo "xdg-terminal-exec nmtui"
@@ -93,7 +97,8 @@ echo "<item label='Network Manager'>"
   echo '</execute></action>'
   echo '</item>'
 
- echo '<menu id="vpn" label="VPN">'
+# VPN STUFF
+echo '<menu id="vpn" label="VPN">'
 
 if [ "$vpn" ]; 
 then
@@ -120,13 +125,13 @@ echo "<item label='Close VPN'>"
   echo '</item>'
 
  echo '</menu> '
+# END VPN STUFF   
  echo '</menu> '
  
-
+# END NETWORK STUFF  
   
 
- echo '<menu id="power" label="Power">'
-
+echo '<menu id="power" label="Power">'
 echo '  <item label="Log Out">'
 echo '    <action name="Exit" />'
 echo '  </item>'
